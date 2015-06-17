fb80e9737743c1fd32a0175d66e40f38e9225763
Tom Broiles <tbroiles@tbroilesmac.space.swri.edu>
2015-06-16 22:26:41 -0500

    HW2 is added to file cacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function (makeCacheMatrix) creates a matrix for testing.
+## The second function (cacheSolve) calculates the inverse of the test 
+## matrix (if possible) or recalls, it if done previously.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a matrix for testing.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv = NULL
+  set <- function(y) { ## sets the test matrix
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x ## recalls the test matrix
+  setinverse <- function(inv) { ## sets inverse of the test matrix
+    if(nrow(x) == ncol(x)) {
+      inv <<- solve(x) 
+    }
+  }
+  getinverse <- function() inv ## recalls the inverse
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the test matrix (if square) or recalls 
+## it if available.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) { ## looks for a recorded inverse first
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  if(ncol(data)==nrow(data)) { ## only compute inverse if x is square
+    inv <- solve(data)
+    x$setinverse(inv)
+  }
+  inv
 }
